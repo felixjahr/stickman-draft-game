8187d795e7eb3fb57c1047466e265141b1a6e1d2
stefan <go67los@mytum.de>
2026-05-09 14:11:31 +0200

    integrate new ability button and adjusted controls layout

--- godot/ui/overlay/overlay.gd
+++ godot/ui/overlay/overlay.gd
@@ -8,12 +8,13 @@ var weapon_aim_directions: Array[Vector2] = [Vector2.ZERO, Vector2.ZERO]
 var direction := 0.0
 var jumping := false
 var ability := false
-var ability_button_pressed := false
+
+var current_ability_id = ""
 
 @onready var aim_joystick_0 := $AimJoystick0
 @onready var aim_joystick_1 := $AimJoystick1
 @onready var dpad := $Dpad
-@onready var ability_button := $Ability/Ability
+@onready var ability_button := $AbilityButton
 
 
 func poll() -> void:
@@ -25,10 +26,16 @@ func poll() -> void:
 	
 	direction = dpad.output.x
 	jumping = dpad.output.y < 0.0
-	ability = Input.is_action_just_pressed("ability") or ability_button.button_pressed
+	ability = Input.is_action_just_pressed("ability") or ability_button.is_pressed()
 
 
 func apply_snapshot(snapshot: PlayerSnapshot) -> void:
+	if current_ability_id != snapshot.ability_id:
+		current_ability_id = snapshot.ability_id
+		ability_button.setup_ability(current_ability_id)
+
 	var total_recharge_time = Data.ABILITY[snapshot.ability_id].recharge_time
 	var recharge_progress := clampf(snapshot.ability_recharge_time / total_recharge_time, 0.0, 1.0)
-	$Ability/ProgressBar.value = (1.0 - recharge_progress) * 100.0
+	var percent = (1.0 - recharge_progress) * 100.0
+	
+	ability_button.update_cooldown_visuals(percent)
